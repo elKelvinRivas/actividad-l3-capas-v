30c816fe846552819a65dd719818b8673130f402
elKelvinRivas <00083522@uca.edu.sv>
2025-06-13 07:27:17 -0600

    Add: Init Project :memo:

---
+++ Videojuegos_Labo3/src/main/java/com/example/videojuegos_labo3/VideojuegosLabo3Application.java
@@ -0,0 +1,13 @@
+package com.example.videojuegos_labo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideojuegosLabo3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideojuegosLabo3Application.class, args);
+    }
+
+}
---
+++ Videojuegos_Labo3/src/test/java/com/example/videojuegos_labo3/VideojuegosLabo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.videojuegos_labo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideojuegosLabo3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
